0a0f1f9abe3a7de3a0ad67ae52a1a4db5d848123
Simon Grätzer <simon@graetzer.org>
2015-04-24 02:50:26 +0200

    Working parser

--- app/src/main/java/de/rwth_aachen/comsys/assignment2/MensaDetailFragment.java
+++ app/src/main/java/de/rwth_aachen/comsys/assignment2/MensaDetailFragment.java
@@ -1,17 +1,30 @@
 package de.rwth_aachen.comsys.assignment2;
 
 import android.app.ListFragment;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.TextView;
 
 
+import java.io.IOException;
+import java.text.NumberFormat;
+import java.util.List;
+import java.util.Locale;
+
+import de.rwth_aachen.comsys.assignment2.data.MealPlan;
 import de.rwth_aachen.comsys.assignment2.data.Mensa;
 
 public class MensaDetailFragment extends ListFragment {
+    private static final String TAG = MensaDetailFragment.class.getSimpleName();
     public static final String ARG_ITEM_ID = "mensa_id";
     private Mensa mMensa;
 
@@ -42,6 +55,92 @@ public class MensaDetailFragment extends ListFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_mensa_detail, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        if (mMensa != null) {
+            new MealPlanTask().execute(mMensa);
+        }
+    }
+
+    private class MealPlanTask extends AsyncTask<Mensa, Void, MealPlan> {
+        ProgressDialog progress;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progress = ProgressDialog.show(getActivity(), "Loading the meal plan",
+                    "please wait a little...", true, false);
+        }
+
+        @Override
+        protected MealPlan doInBackground(Mensa... params) {
+            try {
+                return new MealPlan(getResources().getAssets().open("mensa.html"));
+            } catch (IOException e) {
+                Log.e(TAG, "Error while reading asset mensa.html", e);
+            }
+            return null;
+            //return new MealPlan(params[0].url);
+        }
+
+        @Override
+        protected void onPostExecute(MealPlan mealPlan) {
+            super.onPostExecute(mealPlan);
+            progress.cancel();
+            if (getActivity() != null
+                    && mealPlan != null
+                    && mealPlan.days.size() > 0) {
+                List<MealPlan.Menu> menus = mealPlan.days.get(mealPlan.todayId).menus;
+                setListAdapter(new MenuAdapter(getActivity(), menus));
+            }
+        }
+    }
+
+    private static class MenuAdapter extends BaseAdapter {
+        Context ctx;
+        private List<MealPlan.Menu> mMenus;
+        NumberFormat format = NumberFormat.getCurrencyInstance(Locale.GERMANY);
+
+        public MenuAdapter(Context ctx, List<MealPlan.Menu> menus) {
+            this.ctx = ctx;
+            this.mMenus = menus;
+        }
+
+        @Override
+        public int getCount() {
+            return mMenus.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return mMenus.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = LayoutInflater.from(ctx).inflate(R.layout.course_list_item, parent, false);
+            }
+            TextView categoryTV = (TextView) convertView.findViewById(R.id.textView_category);
+            TextView menuTV = (TextView) convertView.findViewById(R.id.textView_menu);
+            TextView priceTV = (TextView) convertView.findViewById(R.id.textView_price);
+
+            MealPlan.Menu m = mMenus.get(position);
+            categoryTV.setText(m.category);
+            menuTV.setText(m.title);
+            priceTV.setText(format.format(m.price));
+
+            return convertView;
+        }
     }
 
 }
--- app/src/main/java/de/rwth_aachen/comsys/assignment2/data/MealPlan.java
+++ app/src/main/java/de/rwth_aachen/comsys/assignment2/data/MealPlan.java
@@ -1,16 +1,30 @@
 package de.rwth_aachen.comsys.assignment2.data;
 
 import android.net.Uri;
+import android.text.TextUtils;
 import android.util.Log;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.EntityResolver;
+import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
+import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
+import java.io.FilterInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -29,15 +43,16 @@ public class MealPlan {
     private static final String TAG = MealPlan.class.getSimpleName();
 
     public class Day {
-        public String day;
-        List<Item> menus = new ArrayList<>();
+        public String name;
+        public final List<Menu> menus = new ArrayList<>();
     }
 
-    public class Item {
-        String category, menu;
-        double price;
+    public class Menu {
+        public String category, title;
+        public double price;
     }
 
+    public int todayId = 0;
     public final List<Day> days = new ArrayList<>();
 
     /**
@@ -48,7 +63,8 @@ public class MealPlan {
         Uri u = Uri.parse(url);
         byte[] data = requestUrl(u.getHost(), u.getPath());
         try {
-            parseData(data);
+            ByteArrayInputStream in = new ByteArrayInputStream(data);
+            parseData(sanitizeStream(in));
         } catch (Exception e) {
             Log.e(TAG, "Error while parsing data", e);
         }
@@ -56,36 +72,111 @@ public class MealPlan {
 
     /**
      * Probably also don't call this on the main thread
-     * @param data
+     * @param in
      */
-    public MealPlan(byte[] data) {
+    public MealPlan(InputStream in) {
         try {
-            parseData(data);
+            parseData(sanitizeStream(in));
         } catch (Exception e) {
             Log.e(TAG, "Error while parsing data", e);
         }
     }
 
-    private static XPathFactory X_FACTORY = XPathFactory.newInstance();
-    private synchronized void parseData(byte[] data) throws Exception {
+    //private static XPathFactory X_FACTORY = XPathFactory.newInstance();
+    private void parseData(InputStream in) throws Exception {
         DocumentBuilderFactory builderFactory =
                 DocumentBuilderFactory.newInstance();
         DocumentBuilder builder = builderFactory.newDocumentBuilder();
-        Document document = builder.parse(new ByteArrayInputStream(data));
+        builder.setEntityResolver(new EntityResolver() {
+            @Override
+            public InputSource resolveEntity(String publicId, String systemId)
+                    throws SAXException, IOException {
+                if (systemId.contains("foo.dtd")) {
+                    return new InputSource(new StringReader(""));
+                } else {
+                    return null;
+                }
+            }
+        });
 
-        final String weekdays[] = {"montag", "dienstag", "mittwoch", "donnerstag", "freitag"};
+        Document document = builder.parse(in);
+
+        final String weekdayIds[] = {"montag", "dienstag", "mittwoch", "donnerstag", "freitag"};
         final String nextSuffix = "Naechste";
-        XPath xPath =  X_FACTORY.newXPath();
-        String expression = "/Employees/Employee[@emplid='3333']/email";
+        //XPath xPath =  X_FACTORY.newXPath();
 
-        //read a string value
-        Node email = (Node) xPath.compile(expression).evaluate(document, XPathConstants.NODE);
+        for (String dayId : weekdayIds) {
+            parseDayElement(document, dayId);
+        }
+        for (String dayId : weekdayIds) {
+            parseDayElement(document, dayId+nextSuffix);
+        }
+        /*String exp = "/Employees/Employee[@emplid='3333']/email";
+        Node email = (Node) xPath.compile(exp).evaluate(document, XPathConstants.NODE);*/
+    }
 
+    private void parseDayElement(Document document, String dayId) {
+        Day day = new Day();
+        day.name = capitalize(dayId);// Todo
+
+        Element el = document.getElementById(dayId);
+        NodeList trs = el.getElementsByTagName("tr");
+        for (int i = 0; i < trs.getLength(); i++) {
+            Element tr = (Element) trs.item(i);
+            NodeList tds = tr.getElementsByTagName("td");
+
+            MealPlan.Menu menu = new Menu();
+            for (int x = 0; x < tds.getLength(); x++) {
+                Element td = (Element) tds.item(x);
+                String type = td.getAttribute("class");
+                switch (type) {
+                    case "category":
+                        menu.category = sanitize(td.getTextContent());
+                        break;
+                    case "menue":
+                        menu.title = sanitize(td.getTextContent());
+                        break;
+                    case "price":
+                        String in = td.getTextContent();
+                        in = in.replace('€', ' ');
+                        in = in.replace(',', '.');
+                        menu.price = Double.parseDouble(in);
+                        break;
+                }
+            }
+            day.menus.add(menu);
+        }
+        days.add(day);
     }
 
     static {
-        System.loadLibrary("hello"); // Load native library at runtime
-        // hello.dll (Windows) or libhello.so (Unixes)
+        //System.loadLibrary("hello"); // Load native library at runtime
     }
     private native byte[] requestUrl(String host, String path);
+
+    private String capitalize(final String line) {
+        return Character.toUpperCase(line.charAt(0)) + line.substring(1);
+    }
+
+    private String sanitize(String in) {
+        return in != null ? in.trim() : "";
+    }
+
+    /**
+     * No 3th party libraries means we gotta fix the malformed xml/html by ourself
+     * @param in
+     * @return new stream
+     * @throws IOException
+     */
+    private InputStream sanitizeStream(InputStream in) throws IOException {
+        InputStreamReader is = new InputStreamReader(in);
+        StringBuilder sb=new StringBuilder();
+        BufferedReader br = new BufferedReader(is);
+        String read;
+        while((read = br.readLine()) != null) {
+            // See
+            sb.append(read.replace("& ", "&amp; "));
+        }
+        return new ByteArrayInputStream(sb.toString().getBytes("UTF-8"));
+    }
 }
