dc0b4c88d9709bf869537d343aa7c5d4f6e94465
Simon Grätzer <simon@graetzer.org>
2015-04-26 03:54:38 +0200

    Some progress. Parsing mostly works, somehow the headerLength overflows

--- app/src/main/jni/HttpClient.c
+++ app/src/main/jni/HttpClient.c
@@ -1,4 +1,4 @@
-#include "de_rwth_aachen_comsys_assignment2_data_MealPlan.h"
+#//include "de_rwth_aachen_comsys_assignment2_data_MealPlan.h"
 
 #include <sys/time.h>
 #include <sys/types.h>
@@ -6,10 +6,17 @@
 #include <netdb.h>
 #include <arpa/inet.h>
 #include <stdlib.h>
-#include <stdout.h>
+#include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdbool.h>
 #include <zlib.h>
+
+typedef struct {
+	uint8_t *buffer; /// entire request, free this pointer
+	uint8_t *content; /// content
+	size_t bufferLength, contentLength;
+} http_response;
 
 /** @brief Decrompress data compressed with gzip
  *  @param inBuffer ingoing data
@@ -17,26 +24,23 @@
  *  @param outBuffer pointer to a variable which will get the result
  *  @return Size of the output buffer. 0 in case of error
  */
-size_t DecompressGzipBuffer(void *inData, size_t inLength, void** outBuffer) {
+size_t DecompressGzipBuffer(uint8_t *inData, size_t inLength, uint8_t** outBuffer) {
     if (inLength == 0) return 0;
 
+    bool done = false; int status;
     size_t bufferLength = inLength + inLength/2;
-    size_t buffer = malloc(bufferLength);
+    uint8_t* buffer = (uint8_t *)malloc(bufferLength);
     if (!buffer) return 0;
-
-    //NSMutableData *decompressed = [NSMutableData dataWithLength: ];
-    BOOL done = false; int status;
 
     z_stream strm; strm.next_in = (Bytef *)inData; strm.avail_in = inLength;
     strm.total_out = 0; strm.zalloc = Z_NULL; strm.zfree = Z_NULL;
 
-    if (inflateInit2(&strm, (15+32)) != Z_OK)
-        return nil;
+    if (inflateInit2(&strm, (15+32)) != Z_OK) return 0;
 
     while (!done) { // Make sure we have enough room and reset the lengths.
         if (strm.total_out >= bufferLength) {
             bufferLength += inLength/2;
-            buffer = realloc(bufferLength, bufferLength);
+            buffer = (uint8_t *) realloc(buffer, bufferLength);
             if (!buffer) return 0;
         }
         strm.next_out = buffer + strm.total_out;
@@ -113,9 +117,9 @@ int CreateSocketWithOptions(struct addrinfo *host) {
  *  @param path Resource path + query
  *  @return Number of bytes sent, -1 on failure
  */
-ssize_t ConnectAndSendGET(int fd, struct addrinfo *host, const char *host, const char *path) {
+ssize_t ConnectAndSendGET(int fd, struct addrinfo *addr, const char *host, const char *path) {
 	char *request;// asprintf should be available everywhere, alternatively use snprintf
-	int requestLen = asprintf(&buffer,
+	int requestLen = asprintf(&request,
 	"GET %s HTTP/1.1\r\n"
 	"Host: %s\r\n"
 	"User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:37.0) Gecko/20100101 Firefox/37.0\r\n"// Just because
@@ -129,9 +133,9 @@ ssize_t ConnectAndSendGET(int fd, struct addrinfo *host, const char *host, const
 
 	// MSG_FASTOPEN doesn't work on BSD or most other systems
 #ifdef MSG_FASTOPEN
-	ssize_t result = sendto(fd, request, requestLen, MSG_FASTOPEN, host->ai_addr, host->ai_addrlen); // send data with TCP fast open
+	ssize_t result = sendto(fd, request, requestLen, MSG_FASTOPEN, addr->ai_addr, addr->ai_addrlen); // send data with TCP fast open
 #else
-	if (connect(fd, host->ai_addr, host->ai_addrlen) == -1) {
+	if (connect(fd, addr->ai_addr, addr->ai_addrlen) == -1) {
 		printf("error connect");
 		return -1;
 	}
@@ -149,13 +153,12 @@ ssize_t ConnectAndSendGET(int fd, struct addrinfo *host, const char *host, const
 /**
 * Find a line ending CRLF. Returns index of CR
 */
-ssize_t FindLineEnding(void* data, size_t len) {
+uint8_t* FindLineEnding(uint8_t* data, size_t len) {
     size_t i = 0;
-    while(i+1 < len) {
-        if (data[i] == '\r' && data[i+1] == '\n') return i;
-        i+=2;
+    while(++i < len) {
+        if (data[i] == '\r' && data[i+1] == '\n') return data+i;
     }
-    return -1;
+    return 0;
 }
 
 /** @brief Receives a http response using the given socket and prints it to stdout
@@ -163,100 +166,119 @@ ssize_t FindLineEnding(void* data, size_t len) {
  *  @param responseBodyData Pointer to the variable which will get the response buffer. Needs to be free()'d afterwards
  *  @return 0 on success, -1 on failure
  */
-ssize_t ReceiveResponse(int fd, char **responseData) {
-    ssize_t bufferLen = 4096, bytesRcvd = 0, parserOffset = 0, headerLength = 0, contentLength = 0;
+int ReceiveResponse(int fd, http_response *response) {
+    size_t bufferLen = 4096, bytesRcvd = 0, headerLength = 0, contentLength = 0;
     bool usesGzip = false, headerReceived = false;
-    int status = 0;
+    int httpStatus = 0;
 
-	char *buffer = (char *) malloc(bufferLen);
+	uint8_t *buffer = (uint8_t *) malloc(bufferLen);
     if (!buffer) {
         printf("error malloc");
         return -1;
     }
 
-	while(!headerReceived || bytesRcvd < contentLength + headerLength) {
-		ssize_t status = recv(fd, (uint8_t*)buffer + bytesRcvd, bufferLen - bytesRcvd, 0);
-		if(status == -1) {// print user-friendly message on error
+	while(!headerReceived
+	      || (contentLength != 0 && bytesRcvd < contentLength + headerLength)) {
+
+		ssize_t byteCount = recv(fd, buffer + bytesRcvd, bufferLen - bytesRcvd, 0);
+		if(byteCount == -1) {// print user-friendly message on error
 			printf("Error recv");
 			goto error_cleanup;
-		} else if(status == 0) { // print error message on connection close
+		} else if(byteCount == 0) { // print error message on connection close
 			printf("Connection closed by the server.\n");
 			break;
 		}
-		bytesRcvd += status;
+		// We are gonna fill up the buffer
+		bytesRcvd += byteCount;
 		if (bufferLen - bytesRcvd < 512) {// Pretty arbitrary
 		    bufferLen += bytesRcvd/2;
-		    buffer = realloc(buffer, bufferLen);
+		    buffer = (uint8_t *) realloc(buffer, bufferLen);
+		    if (!buffer) goto error_cleanup;
 		}
 
         // Try to parse the header, pretty ugly
-        if (!headerReceived && status > 0) {
-            ssize_t lineEnd = FindLineEnding(buffer + parserOffset, bytesRcvd - parserOffset);
-            if (lineEnd == -1) continue;
+        if (!headerReceived) {
+            uint8_t *lineEnd = FindLineEnding(buffer + headerLength, bytesRcvd - headerLength);
+            if (!lineEnd) continue;
 
-            buffer[lineEnd] = '\0';// Artificially terminate the line
-            if (status == 0) {// Should not be 0 after the status line was received
+            *lineEnd = '\0';// Artificially terminate the line
+            printf("Got header line: '%s'\n", (char*)buffer + headerLength);
+
+            if (httpStatus == 0) {// Should not be 0 after the status line was received
                 // Looking for: "HTTP/1.1 200 OK"
                 char *http = "HTTP/1.";
-                char *pch = strstr(buffer, http);// pointer to
+                char *pch = strstr((char*)buffer, http);// pointer to
                 if (!pch) goto error_cleanup;// should not happen
 
                 // atoi should ignore whitespaces and characters after the status code
-                status = atoi(pch + sizeof(http)+1);
-                printf("Received status %d\n", status);
-                if (status != 200) goto error_cleanup;
+                httpStatus = atoi(pch + sizeof(http)+1);
+                printf("HTTP status %d\n", httpStatus);
+                if (httpStatus != 200) goto error_cleanup;
 
-            } else if (lineEnd + 3 < responseRecv// Check if this is the end of the header
-            && response[lineEnd + 2] == '\r' && response[lineEnd + 3] == '\n') {
-                headerLength = lineEnd + 3 + 1;// lineEnd is a 0 based index
+            } else if (lineEnd+3 < buffer+bytesRcvd// Check if this is the end of the header
+            && lineEnd[2] == '\r'// Technically this should look like "...|0|LF|CR|LF"
+            && lineEnd[3] == '\n') {
                 headerReceived = true;
+                printf("Found blank line\n");
+                //headerLength = (lineEnd-buffer)+4;//skip the last CR|LF|CR|LF
             } else {
+
                 // Looking for something like "Content-Length: 1354"
-                char *pch = strchr(buffer + parserOffset, ':');
-                if (!pch) break;// TODO should not happen
+                char *pch = strchr((char*)buffer + headerLength, ':');
+                if (!pch) goto error_cleanup;
                 *pch = '\0';// Terminate so we can use strcasecmp
 
-                // TODO Could there be leading whitespaces?
-                if (strcasecmp(buffer + parserOffset, "Content-Length") == 0) {
-                    contentLength = atol(pch + 1);// should work?
-                    if (bufferLen - contentLength < 0) {
-                        bufferLen += contentLength - bufferLen;
-                        buffer = realloc(buffer, bufferLen);
+                // TODO Could there be leading whitespaces? Seems to work anyway
+                if (strcasecmp((char*)buffer + headerLength, "Content-Length") == 0) {
+                    contentLength = atol(pch + 1);
+                    printf("Received Content-Length: %zu\n", contentLength);
+                    if (contentLength+bytesRcvd >= bufferLen) {
+                        bufferLen += contentLength+bytesRcvd - bufferLen;
+                        buffer = (uint8_t *) realloc(buffer, bufferLen);
+                        if (!buffer) goto error_cleanup;
                     }
-                } else if (strcasecmp(buffer + parserOffset, "Content-Encoding") == 0) {
+                } else if (strcasecmp((char*)buffer + headerLength, "Content-Encoding") == 0) {
                     // Looking for "Content-Encoding: gzip"
-                    if (strstr(pch + 1, "gzip") != null) {
+                    if (strstr(pch + 1, "gzip")) {
                         usesGzip = true;
                     }
                 }
             }
-            parserOffset = lineEnd+2;
+            headerLength = lineEnd - buffer;//skip the last CRLF
+            headerLength += 2;
+            printf("Buffer %zu; LineEnd %zu\n", (unsigned long)lineEnd, (unsigned long)buffer);
+            printf ("Setting header offset to %zu\n", headerLength);
+            printf ("Total bytes received %zu\n", bytesRcvd);
         }
 	}
 
     // This must hold after every request
 	if (headerReceived) {
+	    response->buffer = buffer;
+	    response->bufferLength = bytesRcvd;
+
 	    if (bytesRcvd > headerLength) {// Got a body
 	        if (usesGzip) {
-        	    size_t responseLength = DecompressGzipBuffer(buffer + headerLength, bytesRcvd - headerLength, &responseData);
+        	    response->contentLength = DecompressGzipBuffer(buffer + headerLength,
+        	                                                   bytesRcvd - headerLength,
+        	                                                   &(response->content));
         	    free(buffer);
-        	    return responseLength;
+        	    return bytesRcvd;
         	} else {
-        	    /*buffer[bytesRcvd-1] = '\0';
-                            printf("%s\n", (const char*)(buffer + headerLength));*/
-
-                // Todo I can't return the offset
-                *responseData = buffer + headerLength;
-                return responseRecv - headerLength;
+        	    response->content = buffer + headerLength;
+        	    response->contentLength = bytesRcvd - headerLength;
         	}
+        	response->content[response->contentLength-1] = '\0';
+        	printf("Body:\n%s\n", (char *)response->content);
         }
-        return 0;// No body received
+        return 0;// All cool
 	}
+	printf("It seems there was no complete header\n");
 
     // Sometimes life just doesn't work out
 error_cleanup:
-    printf("Error cleanup");
-    free(buffer); // free memory
+    printf("Error cleanup\n");
+    free(buffer);
     return -1;
 }
 
@@ -265,12 +287,16 @@ error_cleanup:
  * Method:    requestUrl
  * Signature: (Ljava/lang/String;Ljava/lang/String;)[B
  */
-JNIEXPORT jbyteArray JNICALL Java_de_rwth_1aachen_comsys_assignment2_data_MealPlan_requestUrl
-  (JNIEnv *env, jobject object, jstring serverhost, jstring path) {
+/*JNIEXPORT jbyteArray JNICALL Java_de_rwth_1aachen_comsys_assignment2_data_MealPlan_requestUrl (JNIEnv *env, jobject object, jstring serverhost, jstring path) {
     const char *cServerHost = (*env)->GetStringUTFChars(env, serverhost, 0);
     const char *cPath = (*env)->GetStringUTFChars(env, path, 0);
+    //*/
+int main() {
+    //Testing http://www.studentenwerk-aachen.de/speiseplaene/academica-w.html
+    const char *cServerHost = "www.studentenwerk-aachen.de";
+    const char *cPath = "/speiseplaene/academica-w.html";
 
-    struct addrinfo *host = ResolveHost(cServerHost, 80); // resolve hostname and port
+    struct addrinfo *host = ResolveHost(cServerHost, "80"); // resolve hostname and port
 
     if(!host) // exit if hostname could not be resolved
         return -1;
@@ -281,23 +307,25 @@ JNIEXPORT jbyteArray JNICALL Java_de_rwth_1aachen_comsys_assignment2_data_MealPl
         return -2;
     }
 
-    if(ConnectAndSendData(fd, host, cServerHost, cPath) == -1) { // connect & send request
+    if(ConnectAndSendGET(fd, host, cServerHost, cPath) == -1) { // connect & send request
         close(fd); // close socket
         freeaddrinfo(host); // free addrinfo(s)
         return -3;
     }
 
-    if(ReceiveResponse(fd) == -1) { // receive & print response
+    http_response response;
+    memset(&response, 0, sizeof(http_response));
+    if(ReceiveResponse(fd, &response) == -1) { // receive & print response
         close(fd); // close socket
         freeaddrinfo(host); // free addrinfo(s)
         return -4;
     }
 
+    free(response.buffer);
     close(fd); // close socket
     freeaddrinfo(host); // free addrinfo(s)
 
     // Releasing allocated c strings
-    (*env)->ReleaseStringUTFChars(env, serverhost, cServerHost);
-    (*env)->ReleaseStringUTFChars(env, path, cPath);
-    //(*env)->NewStringUTF(env, "Hello");
-  }
+    //(*env)->ReleaseStringUTFChars(env, serverhost, cServerHost);
+    //(*env)->ReleaseStringUTFChars(env, path, cPath);
+}
