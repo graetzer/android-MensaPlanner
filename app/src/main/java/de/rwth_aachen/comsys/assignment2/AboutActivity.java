70dd925e873495101119fd1481d2194d2b993a9f
Simon Grätzer <simon@graetzer.org>
2015-05-04 15:15:30 +0200

    Fixed about page

--- app/src/main/java/de/rwth_aachen/comsys/assignment2/AboutActivity.java
+++ app/src/main/java/de/rwth_aachen/comsys/assignment2/AboutActivity.java
@@ -2,9 +2,12 @@ package de.rwth_aachen.comsys.assignment2;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NavUtils;
 import android.util.AttributeSet;
+import android.view.MenuItem;
 import android.view.View;
 
 /**
@@ -14,6 +17,28 @@ public class AboutActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Fragment should be included
+        setContentView(R.layout.activity_about);
 
+        // Show the Up button in the action bar.
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            // This ID represents the Home or Up button. In the case of this
+            // activity, the Up button is shown. Use NavUtils to allow users
+            // to navigate up one level in the application structure. For
+            // more details, see the Navigation pattern on Android Design:
+            //
+            // http://developer.android.com/design/patterns/navigation.html#up-vs-back
+            //
+            NavUtils.navigateUpFromSameTask(this);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
